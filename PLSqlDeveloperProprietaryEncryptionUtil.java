187aeda173b63b083f6dd55a70d2752ba5926903
Kevin Urbainczyk <kevin@ptl-clan.de>
2016-11-10 15:46:01 +0100

    Update PLSqlDeveloperProprietaryEncryptionUtil.java
    init

---
+++ PLSqlDeveloperProprietaryEncryptionUtil.java
@@ -0,0 +1,35 @@
+package eu.eurogate.gos.client;
+
+/**
+ * Simple class to generate passwords for the plsql-developer (.connection) files. The Key is stored
+ * within the password (first 4 digits). So since it does not really matters then, its can just be
+ * 2001
+ *
+ * Original calculation is (key between 1 and 999) + 2000.
+ *
+ * Inspired by:
+ * <a href="https://adamcaudill.com/2016/02/02/plsql-developer-nonexistent-encryption/">Adam
+ * Caudill</a>
+ */
+public class PLSqlDeveloperProprietaryEncryptionUtil
+{
+   private PLSqlDeveloperProprietaryEncryptionUtil()
+   {
+      // I won't be an instance :(
+   }
+
+   private static final int KEY = 2001;
+
+   public static final String encrypt(final String raw)
+   {
+      String ret = "";
+      for (int i = 0; i < raw.length(); i++)
+      {
+         final int mask = raw.charAt(i) << 4;
+         final int n = (mask ^ (KEY + ((i + 1) * 10))) + 1000;
+         ret += String.valueOf(n);
+      }
+      return String.valueOf(KEY) + ret;
+   }
+
+}
